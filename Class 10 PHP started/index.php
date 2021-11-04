0c835ac4a41c7666a320e555685fb3e6bdb5445e
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-11-04 17:40:12 +0600

    class 10 done

---
+++ Class 10 PHP started/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <?php 
+    //ECHO FUNCTION
+    echo "2+1";
+    
+    
+    ?>
+    <h1>Hello World from PHP!</h1>
+</body>
+
+
+</html>
