5d008789843abca367ae60264bf12b3820d88df4
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-11-07 17:54:38 +0600

    class 11 added

---
+++ Class 11 PHP variable and data type/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <?php 
+    //DATATYPE LINK https://www.w3schools.com/php/php_datatypes.asp
+    // STRING FUNCTION LINK :https://www.w3schools.com/php/php_ref_string.asp
+    
+    // // variable
+    // $email = 1.5;
+    // echo $email;
+    // echo "<br/>";
+    // echo $email;
+    // echo "<br/>";
+    // echo $email;
+    // echo "<br/>";
+    // echo $email;
+    // echo "<br/>";
+    // echo $email;
+    // echo "<br/>";
+    // echo $email;
+    // ?>
+    <form action=""></form>
+
+    <?php
+    // // Operations
+    //SUM
+    $num1 = 10;
+    $num2 = 5;
+    $sum = $num1 + $num2;
+    echo $sum;
+    echo "<br>";
+
+    //SUB
+    $sub = $num1 - $num2;
+    echo $sub;
+    echo "<br>";
+
+    //MULTIPLICATION
+    $multiple = $num1 * $num2;
+    echo $multiple;
+    echo "<br>";
+
+    //Division
+    $div = $num2 / $num1;
+    echo $div;
+    echo "<br>";
+
+    //Modulus ***
+    $mod=  $num1 % $num2;
+    echo $mod;
+    echo "<br>";
+
+    $str = "Hello World 234";
+    echo chop($str,"234");
+    echo $str.length();
+
+    ?>
+
+    <br> 2nd value <br>
+
+
+
+
+    <?php
+    $email = "ABCD";
+    echo $email;
+    
+
+    $username="";
+    
+    
+    ?>
+</body>
+
+
+</html>
